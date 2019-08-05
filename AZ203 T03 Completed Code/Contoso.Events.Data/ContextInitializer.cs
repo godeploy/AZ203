e7c5d6d09470663da2ed83ac850b16ab02109edd
kramit <mike@michaelwhitehouse.com>
2019-08-05 17:14:52 +0100

    add missing lines

--- AZ203 T03 Completed Code/Contoso.Events.Data/ContextInitializer.cs
+++ AZ203 T03 Completed Code/Contoso.Events.Data/ContextInitializer.cs
@@ -8,21 +8,27 @@ namespace Contoso.Events.Data
 {
     public class ContextInitializer
     {
-        public async Task InitializeAsync(EventsContext eventsContext)
-        {
-            await eventsContext.Database.EnsureCreatedAsync();
-            if (!await eventsContext.Events.AnyAsync()){
-                Event eventItem = new Event();
-                eventItem.EventKey = "FY17SepGeneralConference";
-                eventItem.StartTime = DateTime.Today;
-                eventItem.EndTime = DateTime.Today.AddDays(3d);
-                eventItem.Title = "FY17 September Technical Conference";
-                eventItem.Description = "Sed in euismod mi.";
-                eventItem.RegistrationCount = 1;
-                eventsContext.Events.Add(eventItem);
-            }
-            await eventsContext.SaveChangesAsync();
+        	public async Task InitializeAsync(EventsContext eventsContext)
+{
+await eventsContext.Database.EnsureCreatedAsync();
 
-        }
+if (!await eventsContext.Events.AnyAsync())
+{
+await eventsContext.Events.AddRangeAsync(
+new List<Event>() 
+{
+new Event { EventKey = "GeneralConferenceAlpha", StartTime = DateTime.Today, EndTime = DateTime.Today.AddDays(5d), Title = "First General Conference", Description = "Sed in euismod mi.", RegistrationCount = 15 },
+new Event { EventKey = "GeneralConferenceBravo", StartTime = DateTime.Today.AddDays(10d), EndTime = DateTime.Today.AddDays(15d), Title = "Second General Conference", Description = "Sed in euismod mi.", RegistrationCount = 20 },
+new Event { EventKey = "GeneralConferenceCharlie", StartTime = DateTime.Today.AddDays(20d), EndTime = DateTime.Today.AddDays(25d), Title = "Third General Conference", Description = "Sed in euismod mi.",  RegistrationCount = 5 },
+new Event { EventKey = "GeneralConferenceDelta", StartTime = DateTime.Today.AddDays(30d), EndTime = DateTime.Today.AddDays(35d), Title = "Fourth General Conference", Description = "Sed in euismod mi.", RegistrationCount = 25 },
+new Event { EventKey = "GeneralConferenceEcho", StartTime = DateTime.Today.AddDays(40d), EndTime = DateTime.Today.AddDays(45d), Title = "Fifth General Conference", Description = "Sed in euismod mi.", RegistrationCount = 10 },
+new Event { EventKey = "GeneralConferenceFoxtrot", StartTime = DateTime.Today.AddDays(50d), EndTime = DateTime.Today.AddDays(55d), Title = "Sixth General Conference", Description = "Sed in euismod mi.", RegistrationCount = 0 }
+}
+);
+
+await eventsContext.SaveChangesAsync();
+}
+}
+
     }
 }
