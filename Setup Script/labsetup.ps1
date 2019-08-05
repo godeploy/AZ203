c8e280dffc135d39fd3ce57e68bcef475f540bff
kramit <mike@michaelwhitehouse.com>
2019-08-05 19:28:43 +0100

    Update labsetup.ps1

--- Setup Script/labsetup.ps1
+++ Setup Script/labsetup.ps1
@@ -11,14 +11,14 @@ choco install git -params '"/GitAndUnixToolsOnPath"' -y -f
 Write-Host "Installing latest version of VScode" -ForegroundColor green
 choco install vscode -y -f
 
-Write-Host "pulling latest course files from github" -ForegroundColor green
-Invoke-Expression 'cmd /c start powershell -Command {
-    cd f:\ 
-    "C:\Program Files\Git\cmd\git.exe" clone "https://github.com/microsoftlearning/AZ-203-DevelopingSolutionsForMicrosoftAzure"
-    Write-Host "Linking the coursefile folder" -ForegroundColor green
-    New-Item -ItemType SymbolicLink -target "f:\AZ-203-DevelopingSolutionsForMicrosoftAzure\Allfiles" -path "f:\allfiles"
-    pause
-}'
+Write-Host "pulling latest course files from github to f:\" -ForegroundColor green
+$Env:Path += "C:\Program Files\Git\cmd"
+cd f:\ 
+git clone "https://github.com/microsoftlearning/AZ-203-DevelopingSolutionsForMicrosoftAzure"
+Write-Host "Linking the coursefile folder" -ForegroundColor green
+New-Item -ItemType SymbolicLink -target "f:\AZ-203-DevelopingSolutionsForMicrosoftAzure\Allfiles" -path "f:\allfiles"
+    
+
 
 
 Write-Host "Completed setup" -ForegroundColor green
