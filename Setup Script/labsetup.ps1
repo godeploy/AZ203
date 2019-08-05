1fd6bbda4f53795c911ecd6ee11f12bee5fb82f2
Michael Whitehouse <mike@michaelwhitehouse.com>
2019-08-05 10:09:18 -0700

    Update labsetup.ps1

---
+++ Setup Script/labsetup.ps1
@@ -0,0 +1,22 @@
+if(
+([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+)
+{
+write-host "Getting and installing chocolatey package manager" -ForegroundColor green
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+write-host "Installing latest version of git" -ForegroundColor green
+choco install git -params '"/GitAndUnixToolsOnPath"' -y -f 
+
+Write-Host "Installing latest version of VScode" -ForegroundColor green
+choco install vscode -y -f
+
+Write-Host "pulling latest course files from github" -ForegroundColor green
+cmd.exe /c "git clone https://github.com/microsoftlearning/AZ-203-DevelopingSolutionsForMicrosoftAzure"
+
+Write-Host "Linking the coursefile folder" -ForegroundColor green
+New-Item -ItemType SymbolicLink -target "f:\AZ-203-DevelopingSolutionsForMicrosoftAzure\Allfiles" -path "f:\allfiles"
+}
+else{
+Write-Host "Please run this in an admin shell" -ForegroundColor Yellow
+}
